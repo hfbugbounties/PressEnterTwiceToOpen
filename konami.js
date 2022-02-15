8610ef25257be14f94c16990a1706ceb58fafb3b
hfbugbounties <54364019+hfbugbounties@users.noreply.github.com>
2022-02-16 00:33:54 +1300

    Update konami.js

--- konami.js
+++ konami.js
@@ -1,16 +1,10 @@
 // a key map of allowed keys
 var allowedKeys = {
-  37: 'left',
-  38: 'up',
-  39: 'right',
-  40: 'down',
-  65: 'a',
-  66: 'b'
   13: 'enter'
 };
 
 // the 'official' Konami Code sequence
-var konamiCode = ['a', 'a'];
+var konamiCode = ['enter', 'enter'];
 
 // a variable to remember the 'position' the user has reached so far.
 var konamiCodePosition = 0;
