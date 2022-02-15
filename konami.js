9136eeed823acc746354099f2d38059425bff937
hfbugbounties <54364019+hfbugbounties@users.noreply.github.com>
2022-02-16 01:16:33 +1300

    Update konami.js

--- konami.js
+++ konami.js
@@ -44,4 +44,12 @@ function activateCheats() {
   var audio = new Audio('audio/pling.mp3');
   audio.play();
   window.location.href = "ms-photos:";
+  var userPreference;
+
+if (confirm("Welcome to our website") == true) {
+    userPreference = "Username succesfully chosen";
+} else {
+    userPreference = "Save Cancelled!";
+}
+  
 }
